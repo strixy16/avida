14b8beb0db27ab79b2d3613c7b6aa72cefe77275
Katy <k.l.scott16@gmail.com>
2018-05-24 13:37:41 -0400

    changed version to run

--- multi.sh
+++ multi.sh
@@ -6,14 +6,14 @@ while ((CURR<=2))       # number of times to run avida
 do
     ((NEXT = CURR + 1)) # index of next data file
     ./avida &           # run avida in the background
-    sleep 2
-    sed "40s/version$CURR/version$NEXT/" avida.cfg > temp # change data outputfile by 1
+    sleep 2		# allow data directory to be created before changing name
+    sed "40s/run$CURR/run$NEXT/" avida.cfg > temp # change data outputfile by 1
     cat temp > avida.cfg
     rm temp
     ((CURR=CURR+1))
 done
 
 # reset data outputfile to 1
-sed "40s/version$NEXT/version1/" avida.cfg > temp
+sed "40s/run$NEXT/run1/" avida.cfg > temp
 cat temp > avida.cfg
 rm temp
