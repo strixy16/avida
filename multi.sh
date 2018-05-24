65e27bd6653055c53132e3ac5cbe1bca1b73d16a
Katy <k.l.scott16@gmail.com>
2018-05-24 13:57:56 -0400

    avida output into logfile

--- multi.sh
+++ multi.sh
@@ -5,15 +5,15 @@ CURR=1                  #version of data file you start with
 while ((CURR<=2))       # number of times to run avida
 do
     ((NEXT = CURR + 1)) # index of next data file
-    ./avida &           # run avida in the background
+    ./avida > "logfile$CURR" &           # run avida in the background
     sleep 2
-    sed "40s/version$CURR/version$NEXT/" avida.cfg > temp # change data outputfile by 1
+    sed "40s/run$CURR/run$NEXT/" avida.cfg > temp # change data outputfile by 1
     cat temp > avida.cfg
     rm temp
     ((CURR=CURR+1))
 done
 
 # reset data outputfile to 1
-sed "40s/version$NEXT/version1/" avida.cfg > temp
+sed "40s/run$NEXT/run1/" avida.cfg > temp
 cat temp > avida.cfg
 rm temp
