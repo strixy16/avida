c53c008177b17809285bae3ce8f83cdb8b32cd9c
Katy <k.l.scott16@gmail.com>
2018-05-24 16:33:07 -0400

    change back data

--- multi.sh
+++ multi.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 #SBATCH -c 2
-#SBATCH --mem=1024
-#SBATCH --time=0-0:2:00
+#SBATCH --mem=5120
+#SBATCH --time=0-4:0:00
 
 cd cbuild/work          # go into correct directory
 
+sed "40s/data/data1/" avida.cfg > temp  #change data to data1 for while loop
+cat temp > avida.cfg
+
 CURR=1                  #version of data file you start with
-while ((CURR<=2))       # number of times to run avida
+while ((CURR<=5))       # number of times to run avida
 do
 	((NEXT = CURR + 1)) # index of next data file
 	./avida > "logfile$CURR" &           # run avida in the background
         sleep 2	
-	sed "40s/run$CURR/run$NEXT/" avida.cfg > temp # change data outputfile by 1
+	sed "40s/data$CURR/data$NEXT/" avida.cfg > temp # change data outputfile by 1
 	cat temp > avida.cfg
 	rm temp
 	((CURR=CURR+1))
 done
 
 # reset data outputfile to 1
-sed "40s/run$NEXT/run1/" avida.cfg > temp
+sed "40s/data$NEXT/data1/" avida.cfg > temp
 cat temp > avida.cfg
 rm temp
 wait
