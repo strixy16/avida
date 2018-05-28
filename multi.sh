570a2b305c411f24e50f3b94c4f6315f3b5b8d6d
TaraCarette <terra.the.great@gmail.com>
2018-05-28 14:24:09 -0400

    loop will now match timesrun

--- multi.sh
+++ multi.sh
@@ -11,7 +11,7 @@ CURR=1                  #version of data file you start with
 TIMESRUN=6             #number of times you run avida
 arr=(0.0025 0.005 0.0075 0.01 0.05 0.1) #starts at 0.0025 already in file
 i=0
-while ((CURR<=TIMESRUN))
+while ((CURR<TIMESRUN))
 do
     ((NEXT = CURR + 1)) # index of next data file
     ./avida > "logfile$CURR" &           # run avida in the background
