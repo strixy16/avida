1c6c1f670a5a59a4d77dc5ddc0490027df767cb3
TaraCarette <terra.the.great@gmail.com>
2018-06-04 12:49:01 -0400

    changeConcentration sets up folder correctly for not

--- changeConcentration.sh
+++ changeConcentration.sh
@@ -2,9 +2,7 @@ CONCENTRATION=50 #default concentration is 20, can just change number here
 
 #setting concentration
 cd avida-core/source/cpu #get to directory with cHardwareCPU
-sed "2962s/20/$CONCENTRATION/" cHardwareCPU.cc > temp
-cat temp > cHardwareCPU.cc
-rm temp
+sed -i "2962s/20/$CONCENTRATION/" cHardwareCPU.cc 
 
 cd ../../.. #get back into avida
 ./build_avida #compiles new concentration
@@ -17,18 +15,27 @@ cd workantiadd
 mkdir populations #folder to hold populations that can be loaded
 
 #make default copy mutation 0.0025
-sed "49s/0.0075/0.0025/" avida.cfg > temp
-cat temp > avida.cfg
-rm temp
+sed -i "49s/0.0075/0.0025/" avida.cfg
+
+#make it well mixed
+sed -i "94s/0/4/" avida.cfg
 
 #load population line instead of inject
-sed "17s/Inject default-heads.org/LoadPopulation populations\/detail.spop/" events.cfg > temp
-cat temp > events.cfg
-rm temp
+sed -i "17s/Inject default-heads.org/LoadPopulation populations\/detail.spop/" events.cfg
 
+#print the genotype of the dominant
+sed -i "28s/#/ /" events.cfg
+
+#only have not
+sed -i "/REACTION/s/^/#/" environment.cfg
+sed -i "15s/#//" environment.cfg
+#inflow/outflow on not line
+sed -i "15s/process/:resource=resNOT/" environment.cfg
+
+#get inflow and outflow for resource not
+sed -i "13 a RESOURCE  resNOT:inflow=10:outflow=0.01" environment.cfg
 
 #so next time run this it works
 cd ../../avida-core/source/cpu #get to directory with cHardwareCPU
-sed "2962s/$CONCENTRATION/20/" cHardwareCPU.cc > temp
-cat temp > cHardwareCPU.cc
-rm temp
+sed -i "2962s/$CONCENTRATION/20/" cHardwareCPU.cc
+
