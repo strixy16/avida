d5d85f961aa9a33f93902ee00e14a72a6513d58d
Spencer Kelly <16sek@queensu.ca>
2018-06-18 12:26:20 -0400

    setup complete

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-TASKS=(NOT NAND AND ORN OR ANDN NOR XOR EQU)
+RXNS=(NOT NAND AND ORN OR ANDN NOR XOR EQU)
 INSTR=(Nop-A Nop-B Nop-C If-n-equ If-less Pop Push Swap-stk Swap Shift-r Shift-l Inc Dec Add Sub Nand IO H-alloc H-divide H-copy H-search Mov-head Jmp-head Get-head If-label Set-flow)
 CONC=(20 40 60 80 100)
 
@@ -6,21 +6,31 @@ for INSTR in ${INSTR[*]}
 do
 	for CONC in ${CONC[*]}
 	do
-		cd work$INSTR$CONC
-		sed - i "49s/0.0075/0.0025/" avida.cfg
+		cd work$INSTR$CONC   #go into the specific work folder
+		sed - i "49s/0.0075/0.0025/" avida.cfg  #change its copy mutation
 		mkdir run 
-		mv * run/
+		mv * run/    #move all the files into run
 		CNTR=0
-		for TASKS in ${TASKS[*]}
+		for TASKS in ${RXNS[*]}
 		do
-			CNTR=$((CNTR+1))
-			cp run run$TASKS
-			LINE=$((14+CNTR))
-			sed -i "s/-1/$TASKS/" avida.cfg
-			sed -i "s/Inject default-heads.cfg/LoadPopulation ../work$TASKS/data/detail50000.spop/" events.cfg
+			CNTR=$((CNTR+1))	#gonna need this for the environment file
+			cp run run$RXNS		#make folder for specific task
+			LINE=$((14+CNTR))	#same as CNTR
+			cd run$RXNS
+			sed -i "153s/-1/$RXN/" avida.cfg	#change the required reaction
+			sed -i "17s/Inject default-heads.cfg/LoadPopulation ../work$RXN/data/detail-50000.spop/" events.cfg	#get the events file to load the population
 			for I in 15 16 17 18 19 20 21 22 23 
 			do 
-				if [ I -gt LINE ] || [ I -lt LINE]
+				if [ I -gt LINE ] || [ I -lt LINE]	#
 				then
-					sed -i "{I}s/"
+					sed -i "{I}s/^/#/" environment.cfg
+				fi
+			done 
+			cd ..
+		done 
+		rm -R run/
+		cd ..
+	done 
+done 
+
 
