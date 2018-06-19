d0b5fdfb318b3bcb8cc0422663c5bd6625b01cb9
Spencer Kelly <16sek@queensu.ca>
2018-06-19 15:19:37 -0400

    submissions script

---
+++ submission.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+aINSTR=Set-Flow     #(Nop-A Nop-B Nop-C If-n-equ If-less Pop Push Swap-stk Swap Shift-r Shift-l Inc Dec Add Sub Nand IO H-alloc H-divide H-copy H-search Mov-head Jmp-head Get-head If-label Set-flow)
+aCONC=20   #(20 40 60 80 100)
+for INSTR in ${aINSTR[@]} 
+do
+	for CONC in ${aCONC[@]}
+	do
+		sed -i "11s/work/work$INSTR$CONC/" multi.sh
+		./multi.sh
+		sed -i "11s/work$INSTR$CONC/work/" multi.sh
+	done
+done
+		
