d043e16a76494f2187e4f81c0aaa0cd67f36f771
Katy <k.l.scott16@gmail.com>
2018-06-20 10:50:26 -0400

    Fixed merge issues.

--- submission.sh
+++ submission.sh
@@ -1,28 +1,17 @@
 #!bin/bash
-<<<<<<< HEAD
 aRXNS=(ORN OR ANDN NOR XOR EQU)
 aINSTR=Set-Flow     #(Nop-A Nop-B Nop-C If-n-equ If-less Pop Push Swap-stk Swap Shift-r Shift-l Inc Dec Add Sub Nand IO H-alloc H-divide H-copy H-search Mov-head Jmp-head Get-head If-label Set-flow)
 aCONC=20   #(20 40 60 80 100)
-=======
-aINSTR=(Set-Flow)     #(Nop-A Nop-B Nop-C If-n-equ If-less Pop Push Swap-stk Swap Shift-r Shift-l Inc Dec Add Sub Nand IO H-alloc H-divide H-copy H-search Mov-head Jmp-head Get-head If-label Set-flow)
-aCONC=(20)	#(20 40 60 80 100)
->>>>>>> 99b6773848803bf428f690c6fa5dbc6f3550e63d
+
 for INSTR in ${aINSTR[@]} 
 do
 	for CONC in ${aCONC[@]}
 	do
-<<<<<<< HEAD
 		for RXN in ${aRXNS[@]}
 		do
 			sed -i "11s|work|work$INSTR$CONC/run$RXN|" multi.sh
 			./multi.sh
 			sed -i "11s|work$INSTR$CONC/run$RXN|work|" multi.sh
 		done
-=======
-		sed -i "11s/work/work$INSTR$CONC/" multi.sh
-		./multi.sh &
-		sed -i "11s/work$INSTR$CONC/work/" multi.sh
->>>>>>> 99b6773848803bf428f690c6fa5dbc6f3550e63d
 	done
 done
-		
