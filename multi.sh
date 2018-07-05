463f8697c13e23482de46ddd3610443a98158dbb
TaraCarette <terra.the.great@gmail.com>
2018-07-05 12:03:27 -0400

    Doesn't depend on line numbers and specific values

--- multi.sh
+++ multi.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -c 3
 #SBATCH --mem=3072
-#SBATCH --time=0-4:0:00
+#SBATCH --time=0-6:0:00
 
 #variables to adjust
 TIMESRUN=5             #number of times you run avida
@@ -10,26 +10,24 @@ POPUPDATES=100000		#how many updates
 # go into correct directory
 cd cbuild/work          
 
-CURR=1                  #version of data file you start with
+
 #change updates in event file
-sed -i "37s/100000/$POPUPDATES/" events.cfg 
+sed -i -E "s/[0-9]+ Exit/$POPUPDATES Exit/" events.cfg
 
-sed -i "40s/data/data1/" avida.cfg
+#set up data to run multiple times
+sed -i -E "s/DATA_DIR [[:alnum:]]+/DATA_DIR data1/" avida.cfg
 
+CURR=1                  #version of data file you start with
 while ((CURR<=TIMESRUN))
 do
     ((NEXT = CURR + 1)) # index of next data file
     ./avida > "logfile$CURR" &           # run avida in the background
     sleep 2                              #stop avida overiding itself when change config
-    sed -i "40s/data$CURR/data$NEXT/" avida.cfg # change data outputfile by 1
+    sed -i "s/DATA_DIR data$CURR/DATA_DIR data$NEXT/" avida.cfg # change data outputfile by 1
     ((CURR=CURR+1))
     #put any changes to the config here
 done
 
 # reset data outputfile to 1
-sed -i "40s/data$NEXT/data/" avida.cfg
-
-#reset events to 100000
-sed -i "37s/$POPUPDATES/100000/" events.cfg 
-
+sed -i "s/DATA_DIR data$NEXT/DATA_DIR data/" avida.cfg
 wait
