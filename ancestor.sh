0dac4962819b27bfbe0cf9d43174e752294bf323
Katy <k.l.scott16@gmail.com>
2018-07-05 15:29:46 -0400

    Clean up and removal of unnecessary files

--- ancestor.sh
+++ ancestor.sh
@@ -5,8 +5,7 @@
 #SBATCH --mem=1024
 #SBATCH --time=0-3:0:00
 
-source variables.sh
-aRXNS=${REACTIONS[*]}
+source ./variables.sh
 
 #aRXNS=(NOT NAND AND ORN OR ANDN NOR XOR EQU)
 
@@ -16,26 +15,22 @@ do
 	cp -R work work$RXN
 	cd work$RXN
 
-	# Find line number for reaction in question
-	LINE=$(grep -n " $RXN " environment.cfg | cut -f1 -d:)
-
 	# NOT through ANDN settings
 	if [[ $RXN != "NOR" ]] &&  [[ $RXN != "XOR" ]] && [[ $RXN != "EQU" ]]
 	then
 		# Disable all reactions except desired reaction in environment file 
 		sed -i "s/REACTION/#REACTION/" environment.cfg
-		sed -i "${LINE}s/#REACTION/REACTION/" environment.cfg
-		# NOR, XOR, EQU require all reactions below them to be enabled
+		sed -i "s/#REACTION\s+$RXN/REACTION  $RXN/" environment.cfg
+	# NOR, XOR, EQU require all reactions below them to be enabled
 	elif [[ $RXN == "NOR" ]]
 	then
-		XORline=$(($LINE + 1))
-		EQUline=$(($LINE + 2))
-		sed -i "${XORline}s/REACTION/#REACTION/" environment.cfg
-		sed -i "${EQUline}s/REACTION/#REACTION/" environment.cfg
+		sed -i -E "s/REACTION\s+XOR/#REACTION  XOR/" environment.cfg
+		sed -i -E "s/REACTION\s+EQU/#REACTION  EQU/" environment.cfg
 	elif [[ $RXN == "XOR" ]]
 	then
 		EQUline=$(($LINE + 1))
-		sed -i "${EQUline}s/REACTION/#REACTION/" environment.cfg
+		sed -i -E "s/REACTION\s+EQU/#REACTION  EQU/" environment.cfg
+	#EQU leaves all REACTIONS active
 	else
 		continue
 	fi
--- runAncestor.sh
+++ runAncestor.sh
@@ -7,25 +7,13 @@
 #SBATCH --mem=3072
 #SBATCH --time=0-8:0:00
 
-# Checking that multi.sh is in default state
-# Find cd cbuild/work line number
-LINENUM=$(grep -n "cbuild/work" multi.sh | cut -f1 -d:) 
-# Get line contents
-CHECKLINE=$(sed "${LINENUM}q;d" multi.sh)
-# If something is present after work, remove it
-if [[ $CHECKLINE != "cd cbuild/work " ]]
-then
-	sed -i "${LINENUM}s|work\w*|work|" multi.sh
-fi
-
-source variables.sh
-aRXNS=${REACTIONS[*]}
+source ./variables.sh
 
 #aRXNS=(NAND AND ORN OR ANDN NOR XOR EQU)
 # Start runs for each reaction
 for RXN in ${aRXNS[@]}
 do
-	sed -i "${LINENUM}s|work|work$RXN|" multi.sh
+	sed -i -E "s|cd cbuild/work\w*|cd cbuild/work$RXN|" multi.sh
 	sbatch ./multi.sh
-	sed -i "${LINENUM}s|work$RXN|work|" multi.sh
+	sed -i -E "s|cd cbuild/work$RXN|cd cbuild/work|" multi.sh
 done
