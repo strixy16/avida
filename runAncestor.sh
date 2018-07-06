13893a13853fb2a2ad1e374abb841e3a4942f8f6
Katy <k.l.scott16@gmail.com>
2018-07-06 10:05:43 -0400

    Updated number of runs for specific reactions

--- runAncestor.sh
+++ runAncestor.sh
@@ -7,25 +7,23 @@
 #SBATCH --mem=3072
 #SBATCH --time=0-8:0:00
 
-# Checking that multi.sh is in default state
-# Find cd cbuild/work line number
-LINENUM=$(grep -n "cbuild/work" multi.sh | cut -f1 -d:) 
-# Get line contents
-CHECKLINE=$(sed "${LINENUM}q;d" multi.sh)
-# If something is present after work, remove it
-if [[ $CHECKLINE != "cd cbuild/work " ]]
-then
-	sed -i "${LINENUM}s|work\w*|work|" multi.sh
-fi
-
+# Import aRXNS list
 source variables.sh
-aRXNS=${REACTIONS[*]}
 
-#aRXNS=(NAND AND ORN OR ANDN NOR XOR EQU)
 # Start runs for each reaction
 for RXN in ${aRXNS[@]}
 do
-	sed -i "${LINENUM}s|work|work$RXN|" multi.sh
+	# Run 5 trials for XOR and EQU
+	if [[ $RXN == XOR ]] || [[ $RXN == EQU ]]
+	then
+		sed -i -E "s/TIMESRUN=[0-9]/TIMESRUN=5/" multi.sh
+	# Only run 1 trial for the rest of the reactions
+	else
+		sed -i -E "s/TIMESRUN=[0-9]/TIMESRUN=1/" multi.sh
+	fi
+	
+	# Direct multi into the correct directory
+	sed -i -E "s|cd cbuild/work\w*|cd cbuild/work$RXN|" multi.sh
 	sbatch ./multi.sh
-	sed -i "${LINENUM}s|work$RXN|work|" multi.sh
+	sed -i -E "s|cd cbuiild/work$RXN|cd cbuild/work|" multi.sh
 done
