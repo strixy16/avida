d5b3c1997d3dbf885c2c80cbcb9c5d095a13077e
TaraCarette <terra.the.great@gmail.com>
2018-07-23 12:51:38 -0400

    fix naming bug in patch, needs more testing

--- patch.sh
+++ patch.sh
@@ -6,10 +6,9 @@
 
 source ./variables.sh
 
-#create empty list for each reaction
-for RXN in aRXNS; do
-    RERUN$RXN=()
-done
+#create empty list for each incomplete run
+RERUN=()
+
 
 #loop to create arrays with the incomplete data folders in it, each array a different reaction
 for INSTR in ${aINSTR[@]} 
@@ -31,9 +30,9 @@ do
                     NUMORG=$(cut -d " " -f2 <<< $lastLine)
 
                     if [[ $NUMORG -eq 3600 ]]; then #this assumes population of 3600, will need to change if not true
-                        if [[ $UPDATE -ne $POPUPDATEs ]]; then
+                        if [[ $UPDATE -ne $POPUPDATES ]]; then
                             #need a patch, run got cut off early rather than dying out
-                            RERUN$RXN+=("$INSTR$CONC nomutdata$i") #add info to target incomplete run to array
+                            RERUN+=("$RXN $INSTR$CONC nomutdata$i") #add info to target incomplete run to array
                         fi
                     fi
 
@@ -48,9 +47,9 @@ do
                     NUMORG=$(cut -d " " -f2 <<< $lastLine)
 
                     if [[ $NUMORG -eq 3600 ]]; then #this assumes population of 3600, will need to change if not true
-                        if [[ $UPDATE -ne $POPUPDATEs ]]; then
+                        if [[ $UPDATE -ne $POPUPDATES ]]; then
                             #need a patch, run got cut off early rather than dying out
-                            RERUN$RXN+=("$INSTR$CONC data$i") #add info to target incomplete run to array
+                            RERUN+=("$RXN $INSTR$CONC data$i") #add info to target incomplete run to array
                         fi
                     fi
 
@@ -63,38 +62,38 @@ do
     done
 done
 
+
 #submits single multi.sh runs to replace incomplete runs
-for RXN in aRXNS; do
-    for i in ${RERUN$RXN[@]}; do
-        FOLDER=$(cut -d " " -f1 <<< $i)
-        DATA==$(cut -d " " -f2 <<< $i)
+for i in ${RERUN[@]}; do
+    RUNFOLDER=$(cut -d " " -f1 <<< $i)
+    WORKFOLDER=$(cut -d " " -f2 <<< $i)
+    DATA==$(cut -d " " -f3 <<< $i)
 
-        cd work$FOLDER/run$RXN
-        rm -R $DATA
+    cd work$FOLDER/run$RXN
+    rm -R $DATA
 
-        #multi will create data1 so move conflict out of the way
-        if [[ -f data1 ]]; then
-            mv data1 temporarydatafolder
-        fi
+    #multi will create data1 so move conflict out of the way
+    if [[ -f data1 ]]; then
+        mv data1 temporarydatafolder
+    fi
 
-        cd ../..
+    cd ../..
 
-        #goes into the right folder
-        sed -i -E "s|cbuild/[[:alnum:]]*/*[[:alnum:]]*|cbuild/work$FOLDER/run$RXN|" multi.sh 
-        #only runs once
-        sed -i "/source \.\/variables/a RUNS=1" multi.sh
-        sbatch multi.sh
-        #back to neutral
-        sed -i "s|cbuild/work$FOLDER/run$RXN|cbuild/work|" multi.sh
-        #will run according to variables.sh
-        sed -i "/RUNS=1/d" multi.sh
+    #goes into the right folder
+    sed -i -E "s|cbuild/[[:alnum:]]*/*[[:alnum:]]*|cbuild/work$FOLDER/run$RXN|" multi.sh 
+    #only runs once
+    sed -i "/source \.\/variables/a RUNS=1" multi.sh
+    sbatch multi.sh
+    #back to neutral
+    sed -i "s|cbuild/work$FOLDER/run$RXN|cbuild/work|" multi.sh
+    #will run according to variables.sh
+    sed -i "/RUNS=1/d" multi.sh
 
-        cd work$FOLDER/run$RXN
-        #puts the naming back to normal
-        mv data1 $DATA
-        if [[ -f temporarydatafolder ]]; then
-            mv temporarydatafolder data1
-        fi
+    cd work$FOLDER/run$RXN
+    #puts the naming back to normal
+    mv data1 $DATA
+    if [[ -f temporarydatafolder ]]; then
+        mv temporarydatafolder data1
+    fi
 
-        done
-    done
+done
