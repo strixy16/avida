38c8202c800a647a1094234fcb208ad1649ce39c
strixy16 <k.l.scott16@gmail.com>
2018-07-25 10:45:28 -0400

    Actually fixed compatibility with variables.sh
    Changing RUNS in variables didn't work, overwrite in multi instead and remove afterwards.

--- runAncestor.sh
+++ runAncestor.sh
@@ -9,28 +9,20 @@
 
 source ./variables.sh
 
-# Save number of runs from variables file
-oldRUNS=$(grep "RUNS=" variables.sh | cut -d= -f2)
-
 # Start runs for each reaction
 for RXN in ${aRXNS[@]}
 do
 	# Run 5 trials for XOR and EQU
         if [[ $RXN == XOR ]] || [[ $RXN == EQU ]]
         then
-                sed -i -E "s/RUNS=[0-9]/RUNS=5/" variables.sh
+                sed -i '/source .\/variables/aRUNS=5' multi.sh
         # Only run 1 trial for the rest of the reactions
         else
-                sed -i -E "s/RUNS=[0-9]/RUNS=1/" variables.sh
+                sed -i '/source .\/variables/aRUNS=1' multi.sh
         fi
-	
-	# Reload variables with updated RUNS count
-	source ./variables.sh 
 	
 	sed -i -E "s|cd cbuild/work\w*|cd cbuild/work$RXN|" multi.sh
 	sbatch ./multi.sh
 	sed -i -E "s|cd cbuild/work$RXN|cd cbuild/work|" multi.sh
+	sed -i -E '/RUNS=[0-9]/d' multi.sh
 done
-
-# Return RUNS to value it was set at before this script
-sed -i -E "s/RUNS=[0-9]/RUNS=${oldRUNS}/" variables.sh
