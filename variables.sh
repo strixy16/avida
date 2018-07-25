7d810cfedf8b0076d0f23a662cbf70ce087ca5b0
TaraCarette <terra.the.great@gmail.com>
2018-07-25 11:46:48 -0400

    Don't want it on github

--- variables.sh
+++
@@ -1,6 +0,0 @@
-aRXNS=(NOT NAND AND ORN OR ANDN NOR XOR EQU)
-aINSTR=(NopA NopB NopC Ifnequ Ifless Pop Push Swapstk Swap Shiftr Shiftl Inc Dec Add Sub Nand IO Halloc Hdivide Hcopy Hsearch Movhead Jmphead Gethead Iflabel Setflow)
-aCONC=(20 40 60 80 100)
-POPUPDATES=100000
-RUNS=5
-noMut=false
