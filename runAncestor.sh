7e1db1995915405725a32b30f88076d3aecef584
TaraCarette <terra.the.great@gmail.com>
2018-07-26 15:33:05 -0400

    bug fix in runAncestor for numbers of runs cooperating with variables

--- runAncestor.sh
+++ runAncestor.sh
@@ -7,25 +7,22 @@
 #SBATCH --mem=3072
 #SBATCH --time=0-8:0:00
 
-# Import aRXNS list
-source variables.sh
+source ./variables.sh
 
 # Start runs for each reaction
 for RXN in ${aRXNS[@]}
 do
 	# Run 5 trials for XOR and EQU
-	if [[ $RXN == XOR ]] || [[ $RXN == EQU ]]
-	then
-		sed -i -E "s/TIMESRUN=[0-9]/TIMESRUN=5/" multi.sh
-	# Only run 1 trial for the rest of the reactions
-	else
-		sed -i -E "s/TIMESRUN=[0-9]/TIMESRUN=1/" multi.sh
-	fi
+        if [[ $RXN == XOR ]] || [[ $RXN == EQU ]]
+        then
+                sed -i '/source .\/variables/aRUNS=5' multi.sh
+        # Only run 1 trial for the rest of the reactions
+        else
+                sed -i '/source .\/variables/aRUNS=1' multi.sh
+        fi
 	
-	# Direct multi into the correct directory
 	sed -i -E "s|cd cbuild/work\w*|cd cbuild/work$RXN|" multi.sh
 	sbatch ./multi.sh
-	sed -i -E "s|cd cbuiild/work$RXN|cd cbuild/work|" multi.sh
+	sed -i -E "s|cd cbuild/work$RXN|cd cbuild/work|" multi.sh
+	sed -i -E '/RUNS=[0-9]/d' multi.sh
 done
-
-sed -i -E "s/TIMESRUN=[0-9]/TIMESRUN=5/" multi.sh
