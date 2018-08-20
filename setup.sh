2419f27e3949ee473df075a2c5368d3cc5b3f4d9
Katy <k.l.scott16@gmail.com>
2018-08-20 10:42:18 -0400

    Added avida.cfg energy configuration settings

--- setup.sh
+++ setup.sh
@@ -31,25 +31,38 @@ do
 	for CONC in ${aCONC[*]}
 	do
 		cd work$INSTR$CONC   #go into the specific work folder	
-		
+
 		sed -i -E "s/BIRTH_METHOD [0-9]+/BIRTH_METHOD 4/" avida.cfg # Make population well mixed
 		sed -i -E "s/REQUIRED_REACTION -*[0-9]+/REQUIRED_REACTION 0/" avida.cfg	#change the required reaction
-		
+
+		# Energy settings
+		if [[ $energy==true ]]
+		then
+			# Set mutation rate for energy run
+			sed -i -E "s/COPY_MUT_PROB [0-9]*\.[0-9]*/COPY_MUT_PROB 0.00025/" avida.cfg
+
+			# Turn on energy settings
+			sed -i -E "s/ENERGY_ENABLED [0-9]/ENERGY_ENABLED 1/" avida.cfg
+			sed -i -E "s/ENERGY_GIVEN_ON_INJECT [0-9]*/ENERGY_GIVEN_ON_INJECT 500/" avida.cfg
+			sed -i -E "s/NUM_CYCLES_EXC_BEFORE_0_ENERGY [0-9]*/NUM_CYCLES_EXC_BEFORE_0_ENERGY 1000/" avida.cfg
+			sed -i -E "s/APPLY_ENERGY_METHOD [0-9]*/APPLY_ENERGY_METHOD 1/" avida.cfg
+		fi
+
 		mkdir run 
 		mv $(ls | grep -v run) run/    #move all the files into run without moving run into run
-		
+
 		for RXN in ${aRXNS[*]}
 		do
 			cp -R run run$RXN		#make folder for specific task
 			cd run$RXN
-			
+
 			# Change events to load population from ancestors
 			sed -i "/u begin/c\u begin LoadPopulation ..\/..\/work$RXN\/data\/detail.spop" events.cfg
-			
+
 			# Disable all but the tested reaction in environment file 
 			sed -i "s/REACTION/#REACTION/" environment.cfg
 			sed -i -E "s/#REACTION\s+$RXN /REACTION  $RXN/" environment.cfg
-			
+
 			# Energy settings
 			if [[ $energy==true ]]
 			then
@@ -58,7 +71,7 @@ do
 				then
 					sed -i '14iRESOURCE sunlight:initial=1.0:inflow=10:outflow=0.1' environment.cfg
 				fi
-				
+
 				# Replace reaction line with energy settings
 				# Need to add proper max counts and value for proper resource
 				enRXN=${RXN,,}
